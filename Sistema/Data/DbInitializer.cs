1deaffda061626867aefa41b3e4e7fb415ed926e
Rayner <55637443+RaynerAP0102@users.noreply.github.com>
2020-06-10 23:51:58 -0400

    Total

---
+++ Sistema/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistema.Models;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Inizialize (SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //Buscarn si exite
+            if (context.Estudiante.Any()) 
+            {
+                return;
+            }
+            var estudiantes = new Estudiante[]
+            {
+                new Estudiante{Nombre="Orange", Edad="18", Genero="Femenino"}
+            };
+
+            foreach (Estudiante c in estudiantes)
+            {
+                context.Estudiante.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Sistema/Models/Estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Sistema.Models
+{
+    public class Estudiante
+    {
+        public int id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Edad { get; set; } 
+        public string Genero { get; set; }
+    }
+}
